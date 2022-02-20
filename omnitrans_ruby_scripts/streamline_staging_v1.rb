66029d3092035d0bcb64879f1e9026bfa65da080
Louise Klingen <l.m.klingen@gmail.com>
2022-02-20 13:01:30 +0100

    rb omnitrans file

--- omnitrans_ruby_scripts/streamline_staging_v1.rb
+++ omnitrans_ruby_scripts/streamline_staging_v1.rb
@@ -1,28 +1,39 @@
 # OmniTRANS Job for 'lk_testen_zelf_v2'
 # Created 18/02/2022 17:07:07
 
+
 # Create a Simple StreamLine instance
 streamLine = OtStreamLine.new
 
 
-# NETWORK - INOUT
+
+#--- INPUT -----
+
+## network - static
 streamLine.input.network = [10,10]
 
-
-## OD - INPOUT
+## OD matrix - static
 #streamLine.input.odMatrix = [1,10,10,1]
 
 
-## STAGING
+## OD matrix for staging
 streamLine.input.odMatrix = [1, 10, [10, 20, 30], 1]
 streamLine.input.durations = [1800, 1800, 10800]
 
+
+
+
+
+
+#--- SIMULATION -----
 
 ## duration of simulation, must be sum of durations of matrices
 streamLine.propagation.duration = 14400
 
 
 
+
+# ---- OUTPUT
 
 # Store the loads
 streamLine.output.load = [1,10,100,1,19,1]
