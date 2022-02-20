cfea13dc25680997cb79a37023e8c054e4cfc263
Louise Klingen <l.m.klingen@gmail.com>
2022-02-20 13:03:40 +0100

    get data from db

---
+++ omnistrans_results/getdatafromdb.sql
@@ -0,0 +1,132 @@
+---- AREA ------
+
+SELECT * FROM simple.area
+areanr
+
+SELECT * FROM simple.area1data1
+--
+
+SELECT * FROM simple.area2data1
+--
+
+
+--CENTROID
+SELECT * FROM simple.centroid
+centroidnr
+
+SELECT * FROM simple.centroid1data1
+--
+
+SELECT * FROM simple.centroid2data1
+--
+
+SELECT * FROM simple.centroid5_2data1
+--
+
+
+-- CUBE---
+
+SELECT * FROM simple.cube5data1
+--nr en link naar cube files
+
+
+
+%---NODE---
+
+SELECT * FROM simple.node
+nodenr
+
+SELECT * FROM simple.node1_1data1
+-
+
+SELECT * FROM simple.node1data1
+-
+
+SELECT * FROM simple.node1data2
+-
+
+SELECT * FROM simple.node5data1
+-
+
+SELECT * FROM simple.node
+nodenr
+
+SELECT * FROM simple.node1_1data1
+-
+
+SELECT * FROM simple.node1data1
+-
+
+SELECT * FROM simple.node1data2
+-
+
+SELECT * FROM simple.node2data1
+-
+
+SELECT * FROM simple.node5data1
+-
+
+
+---ROUTE--
+SELECT * FROM simple.route
+- routenr
+
+SELECT * FROM simple.route1data1
+-
+
+
+
+
+-----LINK----
+
+SELECT * FROM simple.link
+lnkr en lengte
+
+
+SELECT * FROM simple.link1_1data1
+--
+
+SELECT * FROM simple.link1_1data2
+linknr direction lans
+
+
+SELECT * FROM simple.link1data1
+--
+
+SELECT * FROM simple.link2_1data1
+--link nr direction
+
+
+SELECT * FROM simple.link3_1data1
+--capacity etc ingestelde waardes
+
+
+SELECT * FROM simple.link5_1data1
+---
+
+
+SELECT * FROM simple.link5_1data2
+--
+
+
+SELECT * FROM simple.link5_1data3
+--
+
+
+SELECT * FROM simple.link5_2data1
+--load over time BELANGRIJKE
+
+
+
+--COUNT
+
+SELECT * FROM simple.count
+--nr
+
+SELECT * FROM simple.count1data1
+--
+
+SELECT * FROM simple.count2data1
+--
+
+SELECT * FROM simple.count6data1
