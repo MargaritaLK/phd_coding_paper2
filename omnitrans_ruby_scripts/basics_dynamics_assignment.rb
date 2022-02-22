eacdac6a8c74e22e8df4cc4eaf9d679cad067345
Louise Klingen <l.m.klingen@gmail.com>
2022-02-22 12:28:38 +0100

    add basic ruby omnitrans scipts

---
+++ omnitrans_ruby_scripts/basics_dynamics_assignment.rb
@@ -0,0 +1,38 @@
+
+#create instance of Streamline
+
+streamline = OtStreamline.new
+
+
+#Purpose #Mode #Time #User #Result #Iteration
+
+
+
+#INPUT
+
+streamline.input.network = [1, 2]
+
+streamLine.input.odMatrix = [1, 2, 2, 1]
+
+
+
+# route set generation (optional)
+
+# route choice (optional)
+
+
+
+#TRAFFIC PROPAGATION
+
+
+
+# OUTPUT
+
+
+
+
+
+
+#ECXECUTE
+
+streamLine.execute
