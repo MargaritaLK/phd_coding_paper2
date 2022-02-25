f6beb6333dedfd4d5206ab0061f2574740f49f7f
Louise Klingen <l.m.klingen@gmail.com>
2022-02-25 14:54:45 +0100

    get osm network

---
+++ omnitrans_ruby_scripts/streamline_staging_v3.rb
@@ -0,0 +1,46 @@
+# OmniTRANS Job for 'MultiModal'
+
+#create instance of Streamline
+
+streamLine = OtStreamLine.new
+
+
+
+#INPUT
+
+streamLine.input.network = [10, 10]
+
+#OD - one- zonder stop
+#streamLine.input.odMatrix = [1, 10, 1000, 1]
+
+
+
+
+## OD - for staging
+streamLine.input.odMatrix = [1, 10, [10, 20, 1000 ], 1]
+streamLine.input.durations = [1800, 1800, 3600]
+
+
+
+# route set generation (optional)
+
+# route choice (optional)
+
+
+
+#TRAFFIC PROPAGATION
+
+streamLine.propagation.duration = 7200
+streamLine.propagation.adjustSegmentLength = true
+
+
+# OUTPUT
+streamLine.output.load = [1, 10, 100, 1, 3, 7 ]
+
+
+
+
+
+#ECXECUTE
+
+streamLine.execute
